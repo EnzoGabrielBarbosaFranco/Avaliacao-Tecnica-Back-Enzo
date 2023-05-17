9ca0d4d9a4242a3eec03913b5905880e7bd9c740
Enzo Gabriel <enzoddos7@gmail.com>
2023-05-17 20:45:56 -0300

    criação de projeto spring-boot java

---
+++ src/main/java/com/desafio/digix/DigixApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.digix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/digix/DigixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.digix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
