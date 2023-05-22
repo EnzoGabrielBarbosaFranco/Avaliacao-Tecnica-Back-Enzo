3f00c1b2454885addbb1ac8c8dd4b826c57ffc19
Enzo Gabriel <enzoddos7@gmail.com>
2023-05-22 15:39:52 -0400

    atualizações

--- src/main/java/com/desafio/digix/services/PontuacaoService.java
+++ src/main/java/com/desafio/digix/services/PontuacaoService.java
@@ -1,11 +1,37 @@
 package com.desafio.digix.services;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.desafio.digix.models.PontuacaoFamilia;
 import com.desafio.digix.repository.PontuacaoFamiliaRepository;
 
+@Service
 public class PontuacaoService {
     private final PontuacaoFamiliaRepository pontuacaoFamiliaRepository;
 
     public PontuacaoService(PontuacaoFamiliaRepository pontuacaoFamiliaRepository) {
         this.pontuacaoFamiliaRepository = pontuacaoFamiliaRepository;
+    }
+
+    public List<PontuacaoFamilia> listarPontuacoes() {
+        return pontuacaoFamiliaRepository.findAll();
+    }
+
+    public PontuacaoFamilia obterPontuacaoPorId(Long id) {
+        Optional<PontuacaoFamilia> pontuacaoFamilia = pontuacaoFamiliaRepository.findById(id);
+        if (pontuacaoFamilia.isPresent()) {
+            return pontuacaoFamilia.get();
+        } else {
+            throw new RuntimeException("Pontuação da Família não encontrada");
+        }
+    }
+
+    public PontuacaoFamilia salvarPontuacao(PontuacaoFamilia pontuacaoFamilia) {
+        return pontuacaoFamiliaRepository.save(pontuacaoFamilia);
+    }
+
+    public void excluirPontuacao(Long id) {
+        pontuacaoFamiliaRepository.deleteById(id);
     }
 }
